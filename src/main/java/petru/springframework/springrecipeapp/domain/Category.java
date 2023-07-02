3e9b6c50c090760eab6cdb5bb5efb3e1e5ca7534
PetruAldea1 <petrualdea2810@gmail.com>
2023-07-02 17:16:31 +0300

    Create Category Entity with Many to Many Relationship with Recipe. Closes #3.

---
+++ src/main/java/petru/springframework/springrecipeapp/domain/Category.java
@@ -0,0 +1,19 @@
+package petru.springframework.springrecipeapp.domain;
+
+import jakarta.persistence.*;
+
+
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+}
